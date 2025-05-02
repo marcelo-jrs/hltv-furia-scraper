c9a27764fda921be30107f126fc1e988c6ab9514
marcelo-jrs <fmarcelocarlos@gmail.com>
2025-05-02 19:04:23 -0300

    testing workflow

--- scrape.py
+++ scrape.py
@@ -4,7 +4,7 @@ from bs4 import BeautifulSoup
 import json
 
 def get_soup(url):
-    service = Service('./chromedriver-win64/chromedriver.exe')
+    service = Service('./chromedriver-linux64/chromedriver.exe')
     driver = webdriver.Chrome(service=service)
     driver.get(url)
 
